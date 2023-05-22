b2f6e5b80dce51fdeb4216110e6c2d448e9e438d
sofiehilge <sofie.hilge@gmail.com>
2023-05-22 10:39:56 +0200

    project structure

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import { Outlet } from "react-router-dom";
 
 function App() {
 
 
   return (
-    <>
-    </>
+    <div className='App'><Outlet/>
+    <h1>HEllo WOrld!</h1>
+    </div>
   )
 }
 
