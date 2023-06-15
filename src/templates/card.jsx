b9ecb7ab103a2e7a5d766e5c5943ed29377be4ea
sofiehilge <sofie.hilge@gmail.com>
2023-06-15 12:50:23 +0200

    link around card

--- src/templates/card.jsx
+++ src/templates/card.jsx
@@ -13,6 +13,7 @@ import dogImg from "../assets/Animal.png";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 
+
 const StyledImg = styled.img`
   object-fit: cover;
   width: ${(props) => props.width}px;
@@ -23,27 +24,34 @@ const StyledFigure = styled.figure`
   display: flex;
   justify-content: center;
   align-items: center;
+  grid-template-columns: 1fr;
 `;
 const StyledCardContainer = styled.article`
-  display: flex;
-  justify-content: space-evenly;
   box-shadow: -5px -5px 10px rgba(255, 255, 255, 0.5),
     5px 5px 10px rgba(170, 170, 204, 0.25),
     10px 10px 20px rgba(170, 170, 204, 0.5), -10px -10px 20px #ffffff;
   border-radius: 32px;
   margin: 26px;
+ 
 `;
 
+const StyledLinkContainer = styled.span`
+  display: flex;
+  flex-direction: row;
+  display: block;
+`
 const StyledColumn = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+ 
 `;
 const StyledRow = styled.div`
   display: flex;
   flex-direction: row;
   width: 200px;
   padding: 5px;
+
 `;
 
 const StyledHeart = styled.span`
@@ -56,6 +64,7 @@ const StyledHeart = styled.span`
   border-radius: 50%;
   height: 40px;
   width: 40px;
+  position: absolute;
 `;
 
 const StyledRowSpace = styled.div`
@@ -82,7 +91,9 @@ const AnimalCard = () => {
   const [bookmarked, setBookmarked] = useState(
     JSON.parse(localStorage.getItem(id))
   );
-  console.log(data.id);
+
+  
+  /* console.log(data.id); */
   return (
     <>
       {error && <ErrorView />}
@@ -93,70 +104,76 @@ const AnimalCard = () => {
         <>
           {data.animals.map((animal) => (
             <StyledCardContainer key={animal.id}>
-              <Link to={`/detailview/${animal.id}`}>
-                <StyledFigure>
-                  <StyledImg
-                    width="124"
-                    height="124"
-                    src={
-                      animal.photos.length > 0 ? animal.photos[0].full : dogImg
-                    }
-                  />
-                </StyledFigure>
-              </Link>
-              <StyledColumn>
-                <StyledRowSpace>
-                  <Heading
-                    title={animal.name}
-                    size="20"
-                    center={false}
-                    fam="'Lato', sans-serif"
-                    as="h2"
-                  />
-                  <StyledHeart
-                    onClick={() => {
-                      setBookmarked(!bookmarked);
-                      bookmarked
-                        ? localStorage.removeItem(id)
-                        : localStorage.setItem(id, JSON.stringify(animal.name));
-                    }}
-                  >
-                    {!bookmarked ? (
-                      <AiOutlineHeart style={{ color: "#BDBDBD" }} />
-                    ) : (
-                      <AiFillHeart />
-                    )}
-                  </StyledHeart>
-                </StyledRowSpace>
-                <StyledRow>
-                  <Icon icon={<FaMapMarkerAlt color="57419d" as="i" />} />
-                  <Paragraf
-                    size="12"
-                    color="828282"
-                    text={animal.contact.address.city}
-                    center={false}
-                    lineheight="18"
-                    fam="'Montserrat', sans-serif;"
-                    as="p"
-                  />
-                </StyledRow>
-                <StyledSpan>
-                  <StyledP
-                    as="p"
-                    size="14"
-                    color="4f4f4f"
-                    fam="'Montserrat', sans-serif;"
-                    center={false}
-                    lineheight="18"
-                  >
-                    {animal.description != null &&
-                    animal.description.length > 100
-                      ? animal.description.split(" ").slice(0, 3).join(" ") +
-                        "..."
-                      : "Click for more details..."}
-                  </StyledP>
-                </StyledSpan>
-              </StyledColumn>
+              <StyledLinkContainer>
+                <Link className="flexcontainer" to={`/detailview/${animal.id}`}>
+                  <StyledFigure>
+                    <StyledImg
+                      width="124"
+                      height="124"
+                      src={
+                        animal.photos.length > 0
+                          ? animal.photos[0].full
+                          : dogImg
+                      }
+                    />
+                  </StyledFigure>
+                  <StyledColumn>
+                    <StyledRowSpace>
+                      <Heading
+                        title={animal.name}
+                        size="20"
+                        center={false}
+                        fam="'Lato', sans-serif"
+                        as="h2"
+                      />
+                    </StyledRowSpace>
+                    <StyledRow>
+                      <Icon icon={<FaMapMarkerAlt color="57419d" as="i" />} />
+                      <Paragraf
+                        size="12"
+                        color="828282"
+                        text={animal.contact.address.city}
+                        center={false}
+                        lineheight="18"
+                        fam="'Montserrat', sans-serif;"
+                        as="p"
+                      />
+                    </StyledRow>
+                    <StyledSpan>
+                      <StyledP
+                        as="p"
+                        size="14"
+                        color="4f4f4f"
+                        fam="'Montserrat', sans-serif;"
+                        center={false}
+                        lineheight="18"
+                      >
+                        {animal.description != null &&
+                        animal.description.length > 100
+                          ? animal.description
+                              .split(" ")
+                              .slice(0, 3)
+                              .join(" ") + "..."
+                          : "Click for more details..."}
+                      </StyledP>
+                    </StyledSpan>
+                  </StyledColumn>
+                </Link>
+              </StyledLinkContainer>
+              <StyledHeart
+                onClick={() => {
+                  setBookmarked(!bookmarked);
+                  bookmarked
+                    ? localStorage.removeItem(id)
+                    : localStorage.setItem(id, JSON.stringify(animal.name));
+                }}
+              >
+                {!bookmarked ? (
+                  <AiOutlineHeart style={{ color: "#BDBDBD" }} />
+                ) : (
+                  <AiFillHeart />
+                )}
+              </StyledHeart>
             </StyledCardContainer>
           ))}
         </>
